544f1ce901ec2dd30456b6078aca4d0be2cc432c
muthuskumar <muthuksubbiah@gmail.com>
2017-11-19 17:13:29 +0530

    Enhancing application to use angular router - WIP

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+    { path: '',   component: HomeComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: '**', component: HomeComponent }
+  ];
+
+@NgModule({
+    declarations: [],
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule],
+    providers: [],
+    bootstrap: []
+  })
+export class AppRoutingModule { }
