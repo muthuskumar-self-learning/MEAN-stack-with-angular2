fd1c40c4432652a6da9d88f592a3f2149259987f
Muthu Kumar S <muthuksubbiah@gmail.com>
2017-12-01 21:11:28 +0530

    Added HttpModule to the application

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { RegisterComponent } from './components/register/register.component';
   ],
   imports: [
       BrowserModule,
+      HttpModule,
       ReactiveFormsModule,
       AppRoutingModule
   ],
